34616e15f60fd41850dea6a3aa062b803bdb0a63
Victoroliveira71 <jc306077@gmail.com>
2026-05-13 00:07:03 -0300

    aprindi a (clicar)

---
+++ aula04/aula02.js
@@ -0,0 +1,9 @@
+var n1 =18
+var z = alert("VOÇÊ NÃO PODE ACESSAR ESSE SITE!")
+window.prompt("Qual sua idade?")
+
+if(prompt < n1){
+    console.log(z)
+}else{
+    console.log(alert("Seja bem vindo"))
+}
---
+++ curso js/aula01.js
@@ -0,0 +1,4 @@
+let nome = victor
+console.log("Curso JS")
+console.log(nome)
+console.log('nome:' + nome)
